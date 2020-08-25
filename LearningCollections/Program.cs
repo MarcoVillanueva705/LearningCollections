36c01b2d9d39eea86f43bbdbb220e4ced2bff70c
marco villanueva <villanuevamarco98@gmail.com>
2020-08-24 20:34:08 -0600

    args to params note line 34

--- LearningCollections/Program.cs
+++ LearningCollections/Program.cs
@@ -30,7 +30,7 @@ namespace LearningCollections
                 for(int i = 0; i < studentNames.Count; i++)
                 {
                     //Name is obj arg 0 as string(studentNames), and Grade is obj arg 1 as int(studentGrades)
-                    //args arr[i] lists the data from the loop
+                    //parameters arr[i] lists the data from the loop
                     Console.WriteLine("Name: {0}, Grade: {1}", studentNames[i], studentGrades[i]);
                 }
             }
